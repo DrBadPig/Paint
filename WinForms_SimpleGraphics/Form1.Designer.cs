15fdae14cdd286afbc4cede1b73962cced797e6f
DrBadPig <stuartkirk14@gmail.com>
2020-08-13 14:47:51 +0300

    added mdi documents with no functioality

--- WinForms_SimpleGraphics/Form1.Designer.cs
+++ WinForms_SimpleGraphics/Form1.Designer.cs
@@ -28,22 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAllAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveCurrentAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeThisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeCurrentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(803, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.createToolStripMenuItem.Text = "Create";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAllAsToolStripMenuItem,
+            this.saveCurrentAsToolStripMenuItem});
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.saveAsToolStripMenuItem.Text = "Save";
+            // 
+            // saveAllAsToolStripMenuItem
+            // 
+            this.saveAllAsToolStripMenuItem.Name = "saveAllAsToolStripMenuItem";
+            this.saveAllAsToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.saveAllAsToolStripMenuItem.Text = "Save all as";
+            // 
+            // saveCurrentAsToolStripMenuItem
+            // 
+            this.saveCurrentAsToolStripMenuItem.Name = "saveCurrentAsToolStripMenuItem";
+            this.saveCurrentAsToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.saveCurrentAsToolStripMenuItem.Text = "Save current as";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeThisToolStripMenuItem,
+            this.closeCurrentToolStripMenuItem});
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // closeThisToolStripMenuItem
+            // 
+            this.closeThisToolStripMenuItem.Name = "closeThisToolStripMenuItem";
+            this.closeThisToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.closeThisToolStripMenuItem.Text = "Close all";
+            // 
+            // closeCurrentToolStripMenuItem
+            // 
+            this.closeCurrentToolStripMenuItem.Name = "closeCurrentToolStripMenuItem";
+            this.closeCurrentToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.closeCurrentToolStripMenuItem.Text = "Close current";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 398);
+            this.ClientSize = new System.Drawing.Size(803, 431);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Text = "Finger Drawing";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAllAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveCurrentAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeThisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeCurrentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
     }
 }
 
